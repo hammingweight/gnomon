2880a46febb9a748d6a7f8bc0af9aaa77457e91d
Carl Meijer <hammingweight@gmail.com>
2025-03-20 17:50:04 +0200

    Be less enthusiastic about bumping up the battery capacity.

--- handlers/soc.go
+++ handlers/soc.go
@@ -80,7 +80,7 @@ L:
 	if maxSoc == 100 {
 		threshold = 9 * threshold / 10
 	} else {
-		r := math.Pow(100.0/float64(maxSoc), 0.8)
+		r := math.Pow(100.0/float64(maxSoc), 0.5)
 		newThreshold := int(r*float64(threshold) + 0.5)
 		if newThreshold-threshold < 1 {
 			newThreshold = threshold + 1
